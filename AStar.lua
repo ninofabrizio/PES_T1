ed2d719bb865a55e76fcf8f118679adf417022b5
ninofabrizio <nin0_w3@hotmail.com>
2017-04-06 10:57:07 -0300

    Random walls function added, map generated a bit better

--- AStar.lua
+++ AStar.lua
@@ -3,35 +3,11 @@
 -- Version:
 -- 		1		06/04/2017		-Implemented function to build a map
 --								-Implemented function to print map
+--								-Implemented function to generate random walls in the map
 
 local map = {}
 local height = 10
 local width = 10
-
-function buildMap()
-
-	local mapDimension = height * width
-
-	for i = 1, height do
-    
-    	map[i] = {}
-
-    	for j = 1, width do
-
-    		if j == 1 or j == height then
-        		map[i][j] = 'W'
-    		elseif i == (height - 1) and j == 2 then
-    			map[i][j] = 'I'
-    		elseif i == 2 and j == (width - 1) then
-    			map[i][j] = 'X'
-    		else
-    			map[i][j] = 'W'
-    		end
-    	end
-    end
-
-    printMap()
-end
 
 function printMap()
 
@@ -43,8 +19,84 @@ function printMap()
 
     	io.write("\n")
     end
-
     io.write("\n")
+end
+
+function generateRandomWalls( heightLimit, widthLimit )
+
+	local maxWallNum = 10
+
+	-- Just a (bad) way to avoid a possible infinite loop if there's no way to put more walls in the map
+	local maxNumTries = heightLimit * widthLimit
+
+	while maxWallNum > 0 do
+
+		local xPos
+		local yPos
+
+		repeat
+			math.randomseed(os.time())
+			math.random(); math.random(); math.random()
+			xPos = math.random(2, heightLimit)
+			math.random(); math.random(); math.random()
+			yPos = math.random(2, widthLimit)
+		until map[xPos][yPos] == '.'
+
+		for i = 2, heightLimit do
+
+			local validPosition = false
+
+			if i ~= xPos and map[i][yPos] == '.' then
+
+				for j = 2, widthLimit do
+					if j ~= yPos and map[xPos][j] == '.' then
+						map[xPos][yPos] = 'W'
+						validPosition = true
+						break
+					end
+				end
+			end
+
+			if validPosition then
+				maxWallNum = maxWallNum - 1
+				break
+			end
+    	end
+
+    	maxNumTries = maxNumTries - 1
+
+    	if maxNumTries == 0 then
+    		break
+    	end
+	end
+end
+
+function buildMap()
+
+	local heightLimit = height - 1
+	local widthLimit = width - 1
+
+	for i = 1, height do
+    
+    	map[i] = {}
+
+    	for j = 1, width do
+
+    		if j == 1 or j == height or i == 1 or i == width then
+        		map[i][j] = 'W'
+    		elseif i == heightLimit and j == 2 then
+    			map[i][j] = 'S'
+    		elseif i == 2 and j == widthLimit then
+    			map[i][j] = 'X'
+    		else
+    			map[i][j] = '.'
+    		end
+    	end
+    end
+
+    generateRandomWalls(heightLimit, widthLimit)
+
+    printMap()
 end
 
 -- Main execution
--- README.md
+++ README.md
@@ -4,6 +4,17 @@ Autor: Nino Fabrizio Tiriticco Lizardo
 
 Uma implementação do algoritmo de busca AStar em Lua, utilizando as regras seguidas pelo Engenheiro de Software.
 
+Feito em Lua 5.1.4
+
+------------------------------
+Para executar:
+- Abrir janela do CMD (Windows) ou Terminal (Linux)
+- Posicionar a linha de comando no endereço onde se encontra o projeto na máquina
+- Digitar "lua AStar.lua"
+ou
+- Executar o terminal de Lua digitando "Lua"
+- Digitar "dofile('AStar.lua')"
+
 ------------------------------
 
 Referência:
